9a7315bef212b4a9ad726a0507e261b508f2cabc
Tanvir286 <tanvirdiu200@gmail.com>
2024-03-05 01:12:27 +0600

    react js

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+
+
+
+
+import Title from "./component/Title";
+import Resume from "./component/resume/Resume";
+import ResumeItem from "./component/resume/ResumeItem";
+import Service from "./component/service/Service";
+
+
+
+const App = () => {
+  return (
+    <>
+      
+      
+       <Service />
+
+       <Resume/>
+
+        
+       
+ 
+    </>
+
+  );
+};
+
+export default App;
